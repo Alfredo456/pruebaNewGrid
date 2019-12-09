5417a56b9dd3043fa7da41a84dd854ed50ac2748
Alfredo <alfredo.casique456@gmail.com>
2019-12-09 03:01:23 -0400

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-import { IgxGridModule } from 'igniteui-angular';
+import { IgxGridModule, IgxAvatarModule, IgxBadgeModule, IgxButtonModule, IgxIconModule, IgxInputGroupModule, IgxProgressBarModule, IgxRippleModule, IgxSwitchModule } from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -17,6 +19,18 @@ import { IgxGridModule } from 'igniteui-angular';
     AppRoutingModule,
     BrowserAnimationsModule,
     IgxGridModule.forRoot(),
+
+    FormsModule,
+    IgxAvatarModule,
+    IgxBadgeModule,
+    IgxButtonModule,
+    IgxGridModule,
+    IgxIconModule,
+    IgxInputGroupModule,
+    IgxProgressBarModule,
+    IgxRippleModule,
+    IgxSwitchModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
